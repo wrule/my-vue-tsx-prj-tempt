0977f8dee62bd7ac4388b430bd8d3267e517701e
jimao <1982775886@qq.com>
2021-01-19 11:21:20 +0800

    探索自动完成，搜索，对比

--- src/components/cmeditor/index.tsx
+++ src/components/cmeditor/index.tsx
@@ -3,11 +3,35 @@ import { VNode } from 'vue';
 import style from './index.module.scss';
 import CodeMirror from 'codemirror';
 import 'codemirror/lib/codemirror.css';
+
+// 语法高亮支持
 import 'codemirror/mode/javascript/javascript.js';
+import 'codemirror/mode/xml/xml.js';
+import 'codemirror/mode/clike/clike.js';
+import 'codemirror/mode/sql/sql.js';
+import 'codemirror/mode/shell/shell.js';
+
+// 代码折叠支持
+import 'codemirror/addon/fold/foldgutter.css';
+import 'codemirror/addon/fold/brace-fold.js';
+import 'codemirror/addon/fold/comment-fold.js';
+import 'codemirror/addon/fold/foldcode.js';
+import 'codemirror/addon/fold/foldgutter.js';
+import 'codemirror/addon/fold/indent-fold.js';
+import 'codemirror/addon/fold/markdown-fold.js';
+import 'codemirror/addon/fold/xml-fold.js';
+
+import 'codemirror/addon/hint/show-hint.css';
+import 'codemirror/addon/hint/show-hint.js';
+import 'codemirror/addon/hint/html-hint.js';
+import 'codemirror/addon/hint/xml-hint.js';
+import 'codemirror/addon/hint/sql-hint.js';
+import 'codemirror/addon/hint/anyword-hint.js';
 
 @Component
 export default class XCMEditor extends Vue {
   @Prop({ default: '' }) private readonly text!: string;
+  @Prop({ default: 'text/html' }) private readonly mode!: string;
 
   private codeMirror!: CodeMirror.EditorFromTextArea;
 
@@ -34,7 +58,9 @@ export default class XCMEditor extends Vue {
       this.codeMirror = CodeMirror.fromTextArea(textarea, {
         lineNumbers: true,
         matchBrackets: true,
-        mode: 'text/typescript',
+        mode: this.mode,
+        foldGutter: true,
+        gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
       } as any);
       this.setValue(this.text)
       this.codeMirror.on('change', (codeMirror) => {
--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -8,16 +8,120 @@ import XCMEditor from '@/components/cmeditor';
 @Component
 export default class ViewHome extends Vue {
   private text = `
-private mounted() {
-  const textarea = this.$el.querySelector('textarea');
-  if (textarea) {
-    this.codeMirror = CodeMirror.fromTextArea(textarea, {
-      lineNumbers: true,
-      matchBrackets: true,
-      mode: 'text/typescript'
-    } as any);
-  }
-}`.trim();
+  <?xml version="1.0" encoding="UTF-8"?><jmeterTestPlan version="1.2" properties="4.0" jmeter="4.0 r1823414">
+  <hashTree>
+    <TestPlan guiclass="TestPlanGui" testclass="TestPlan" testname="Test Plan" enabled="true">
+      <stringProp name="TestPlan.comments"/>
+      <boolProp name="TestPlan.functional_mode">false</boolProp>
+      <boolProp name="TestPlan.tearDown_on_shutdown">true</boolProp>
+      <boolProp name="TestPlan.serialize_threadgroups">false</boolProp>
+      <elementProp name="TestPlan.user_defined_variables" elementType="Arguments" guiclass="ArgumentsPanel" testclass="Arguments" testname="User Defined Variables" enabled="true">
+        <collectionProp name="Arguments.arguments"/>
+      </elementProp>
+      <stringProp name="TestPlan.user_define_classpath"/>
+    </TestPlan>
+    <hashTree>
+      <ThreadGroup guiclass="ThreadGroupGui" testclass="ThreadGroup" testname="Thread Group" enabled="true">
+        <stringProp name="ThreadGroup.on_sample_error">continue</stringProp>
+        <elementProp name="ThreadGroup.main_controller" elementType="LoopController" guiclass="LoopControlPanel" testclass="LoopController" testname="Loop Controller" enabled="true">
+          <boolProp name="LoopController.continue_forever">false</boolProp>
+          <stringProp name="LoopController.loops">1</stringProp>
+        </elementProp>
+        <stringProp name="ThreadGroup.num_threads">1</stringProp>
+        <stringProp name="ThreadGroup.ramp_time">1</stringProp>
+        <boolProp name="ThreadGroup.scheduler">false</boolProp>
+        <stringProp name="ThreadGroup.duration"/>
+        <stringProp name="ThreadGroup.delay"/>
+      </ThreadGroup>
+      <hashTree>
+        
+        
+        
+        
+        
+        
+        
+        
+      <CounterConfig guiclass="CounterConfigGui" testclass="CounterConfig" testname="计数器" enabled="true">
+          <stringProp name="CounterConfig.start">1</stringProp>
+          <stringProp name="CounterConfig.end">1111</stringProp>
+          <stringProp name="CounterConfig.incr">1</stringProp>
+          <stringProp name="CounterConfig.name"/>
+          <stringProp name="CounterConfig.format"/>
+          <boolProp name="CounterConfig.per_user">false</boolProp>
+        </CounterConfig><hashTree/><TransactionController guiclass="TransactionControllerGui" testclass="TransactionController" testname="192.168.0.1下单点点滴滴的请求33333333" enabled="true"><boolProp name="TransactionController.includeTimers">false</boolProp><boolProp name="TransactionController.parent">true</boolProp></TransactionController><hashTree><HTTPSamplerProxy guiclass="HttpTestSampleGui" testclass="HTTPSamplerProxy" testname="192.168.0.1下单点点滴滴的请求33333333" enabled="true">
+          <elementProp name="HTTPsampler.Arguments" elementType="Arguments" guiclass="HTTPArgumentsPanel" testclass="Arguments" testname="User Defined Variables" enabled="true">
+            <collectionProp name="Arguments.arguments"/>
+          </elementProp>
+          <stringProp name="HTTPSampler.domain">192.168.0.200</stringProp>
+          <stringProp name="HTTPSampler.port">31026</stringProp>
+          <stringProp name="HTTPSampler.protocol">http</stringProp>
+          <stringProp name="HTTPSampler.contentEncoding"/>
+          <stringProp name="HTTPSampler.path">/order</stringProp>
+          <stringProp name="HTTPSampler.method">GET</stringProp>
+          <boolProp name="HTTPSampler.follow_redirects">true</boolProp>
+          <boolProp name="HTTPSampler.auto_redirects">false</boolProp>
+          <boolProp name="HTTPSampler.use_keepalive">true</boolProp>
+          <boolProp name="HTTPSampler.DO_MULTIPART_POST">false</boolProp>
+          <stringProp name="HTTPSampler.embedded_url_re"/>
+          <stringProp name="HTTPSampler.connect_timeout"/>
+          <stringProp name="HTTPSampler.response_timeout"/>
+        </HTTPSamplerProxy><hashTree><ConstantTimer guiclass="ConstantTimerGui" testclass="ConstantTimer" testname="Constant Timer" enabled="true">
+    <stringProp name="ConstantTimer.delay">0</stringProp>
+</ConstantTimer></hashTree></hashTree><TransactionController guiclass="TransactionControllerGui" testclass="TransactionController" testname="192.168.0.2" enabled="true"><boolProp name="TransactionController.includeTimers">false</boolProp><boolProp name="TransactionController.parent">true</boolProp></TransactionController><hashTree><HTTPSamplerProxy guiclass="HttpTestSampleGui" testclass="HTTPSamplerProxy" testname="192.168.0.2" enabled="true">
+          <elementProp name="HTTPsampler.Arguments" elementType="Arguments" guiclass="HTTPArgumentsPanel" testclass="Arguments" testname="User Defined Variables" enabled="true">
+            <collectionProp name="Arguments.arguments"/>
+          </elementProp>
+          <stringProp name="HTTPSampler.domain">192.168.0.200</stringProp>
+          <stringProp name="HTTPSampler.port">31026</stringProp>
+          <stringProp name="HTTPSampler.protocol">http</stringProp>
+          <stringProp name="HTTPSampler.contentEncoding"/>
+          <stringProp name="HTTPSampler.path">/order</stringProp>
+          <stringProp name="HTTPSampler.method">GET</stringProp>
+          <boolProp name="HTTPSampler.follow_redirects">true</boolProp>
+          <boolProp name="HTTPSampler.auto_redirects">false</boolProp>
+          <boolProp name="HTTPSampler.use_keepalive">true</boolProp>
+          <boolProp name="HTTPSampler.DO_MULTIPART_POST">false</boolProp>
+          <stringProp name="HTTPSampler.embedded_url_re"/>
+          <stringProp name="HTTPSampler.connect_timeout"/>
+          <stringProp name="HTTPSampler.response_timeout"/>
+        </HTTPSamplerProxy><hashTree><ConstantTimer guiclass="ConstantTimerGui" testclass="ConstantTimer" testname="Constant Timer" enabled="true">
+    <stringProp name="ConstantTimer.delay">0</stringProp>
+</ConstantTimer></hashTree></hashTree><TransactionController guiclass="TransactionControllerGui" testclass="TransactionController" testname="192.168.0.1下单点点滴滴的请求33333335555" enabled="true"><boolProp name="TransactionController.includeTimers">false</boolProp><boolProp name="TransactionController.parent">true</boolProp></TransactionController><hashTree><HTTPSamplerProxy guiclass="HttpTestSampleGui" testclass="HTTPSamplerProxy" testname="192.168.0.1下单点点滴滴的请求33333335555" enabled="true">
+          <elementProp name="HTTPsampler.Arguments" elementType="Arguments" guiclass="HTTPArgumentsPanel" testclass="Arguments" testname="User Defined Variables" enabled="true">
+            <collectionProp name="Arguments.arguments"/>
+          </elementProp>
+          <stringProp name="HTTPSampler.domain">192.168.0.200</stringProp>
+          <stringProp name="HTTPSampler.port">31026</stringProp>
+          <stringProp name="HTTPSampler.protocol">http</stringProp>
+          <stringProp name="HTTPSampler.contentEncoding"/>
+          <stringProp name="HTTPSampler.path">/order</stringProp>
+          <stringProp name="HTTPSampler.method">GET</stringProp>
+          <boolProp name="HTTPSampler.follow_redirects">true</boolProp>
+          <boolProp name="HTTPSampler.auto_redirects">false</boolProp>
+          <boolProp name="HTTPSampler.use_keepalive">true</boolProp>
+          <boolProp name="HTTPSampler.DO_MULTIPART_POST">false</boolProp>
+          <stringProp name="HTTPSampler.embedded_url_re"/>
+          <stringProp name="HTTPSampler.connect_timeout"/>
+          <stringProp name="HTTPSampler.response_timeout"/>
+        </HTTPSamplerProxy><hashTree><ConstantTimer guiclass="ConstantTimerGui" testclass="ConstantTimer" testname="Constant Timer" enabled="true">
+    <stringProp name="ConstantTimer.delay">0</stringProp>
+</ConstantTimer></hashTree></hashTree></hashTree>
+      <Arguments guiclass="ArgumentsPanel" testclass="Arguments" testname="User Defined Variables" enabled="true">
+        <collectionProp name="Arguments.arguments">
+          <elementProp name="1" elementType="Argument">
+            <stringProp name="Argument.name">1</stringProp>
+            <stringProp name="Argument.value">3333</stringProp>
+            <stringProp name="Argument.desc">哈哈哈</stringProp>
+            <stringProp name="Argument.metadata">=</stringProp>
+          </elementProp>
+        </collectionProp>
+      </Arguments>
+      <hashTree/>
+    </hashTree>
+  </hashTree>
+</jmeterTestPlan>
+`.trim();
 
   private mounted() {
     // setInterval(() => {
@@ -28,12 +132,12 @@ private mounted() {
   public render(): VNode {
     return (
       <div class={style.view}>
-        <img src={logo} />
+        {/* <img src={logo} />
         <br />
         <span>这是我的主页</span>
         <br />
         <XHello />
-        <br />
+        <br /> */}
         <div class={style.box}>
           <XCMEditor text={this.text} />
         </div>
