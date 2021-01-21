5056a6bc78997dc5260e22c27d5d1f4468ef4276
jimao <1982775886@qq.com>
2021-01-21 21:34:01 +0800

    修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,18 @@
 const webpack = require('webpack');
-const MonacoWebpackPlugin = require('monaco-editor-webpack-plugin');
+const MonacoWebpackPlugin = require('monaco-editor-esm-webpack-plugin');
 
 module.exports = {
-  devServer: {
-    open: true,
-    // proxy: { },
-  },
   configureWebpack: {
+    module: {
+      rules: [
+        {
+          test: /\.js/,
+          enforce: 'pre',
+          include: /node_modules[\\\/]monaco-editor[\\\/]esm/,
+          use: MonacoWebpackPlugin.loader,
+        },
+      ],
+    },
     plugins: [new MonacoWebpackPlugin({
       languages: [
         'xml',
@@ -22,5 +28,9 @@ module.exports = {
         'mysql',
       ],
     })]
+  },
+  devServer: {
+    open: true,
+    // proxy: { },
   },
 };
