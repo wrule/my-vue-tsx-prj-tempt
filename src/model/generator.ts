6e503a6fbe4638fcc5b9a8a589e32b558b8f84ae
jimao <1982775886@qq.com>
2021-02-07 12:17:20 +0800

    提交

--- src/model/generator.ts
+++ src/model/generator.ts
@@ -1,7 +1,45 @@
 import { IFormOut } from "./formOut";
 import { IConfig } from "./iConfig";
 import { IFormIn } from "./iFormIn";
+import Shuji from '@wrule/shuji';
+import URIJS from 'urijs';
 
+const shuji = new Shuji();
+
+function getName(form: IFormIn) {
+  let rst = '';
+  try {
+    if (form.customName.trim()) {
+      rst = form.customName.trim();
+    } else {
+      rst = URIJS(form.apiPath).filename();
+    }
+  } catch (e) {
+    console.error(e);
+  }
+  return rst || 'xxx';
+}
+
+function getDefCode(
+  form: IFormIn,
+  config: IConfig,
+  rspName: string,
+  reqName: string,
+) {
+  const name = getName(form);
+  const pathString = '';
+  const hasReq = form.reqJson.trim().length > 0;
+  const isGet = form.apiMethod === 'get';
+  return `
+/**
+ * 此文件为index.ts，indexd.ts为同目录下模型声明文件
+ */
+import * as ${config.defName} from './indexd';
+
+export const ${name} = (params: ${hasReq ? `${config.defName}.${reqName}` : 'any'}): ${config.defName}.${rspName} =>
+  ${config.axiosName}.${form.apiMethod}('${pathString}', ${isGet && '{ '}params${isGet && ' }'}) as any;
+`.trim();
+}
 
 export function Generate(
   form: IFormIn,
@@ -13,8 +51,15 @@ export function Generate(
     useCode: '',
     importCode: '',
   };
+  const name = getName(form);
 
-
+  if (form.rspJson.trim()) {
+    rst.decCode += shuji.Infer(`${name}Rsp`, JSON.parse(form.rspJson));
+  }
+  if (form.reqJson.trim()) {
+    rst.decCode += '\n\n';
+    rst.decCode += shuji.Infer(`${name}Rep`, JSON.parse(form.reqJson));
+  }
 
   return rst;
 }
