3a244273f395d4d68b53332d1708bee50ec0e474
jimao <1982775886@qq.com>
2021-02-08 11:50:29 +0800

    提交

--- src/components/tsApiHelper/index.tsx
+++ src/components/tsApiHelper/index.tsx
@@ -4,58 +4,70 @@ import { VNode } from 'vue';
 import style from './index.module.scss';
 import XCodeEditor from '@/components/codeEditor';
 import Shuji from '@wrule/shuji';
-import URIJS from 'urijs';
-import { IFormOut } from '@/model/formOut';
-import { IFormIn } from '@/model/iFormIn';
-import { Generate } from '@/model/generator';
-import { IConfig } from '@/model/iConfig';
 
 @Component
 export default class XTsApiHelper extends Vue {
 
   private shuji = new Shuji();
 
-  private config: IConfig = {
-    decName: 'D',
-    apiName: 'API',
-    axiosName: 'http',
-    apiPathTrimNum: 1,
-  };
-
-  private formIn: IFormIn = {
-    apiMethod: 'post',
+  private formIn = {
     apiPath: '',
-    rspJson: JSON.stringify(JSON.parse(
+    responseJSON: JSON.stringify(JSON.parse(
       `{"extParams":{},"object":{"defaultScriptType":"JMETER","isOnline":false,"isProtect":true,"pressAppId":"15100275897401344"},"success":true}`
     ), null, 2),
-    reqJson: '',
-    customName: '',
-  };
-
-  private formOut: IFormOut = {
-    decCode: '',
-    defCode: '',
-    useCode: '',
-    importCode: '',
+    requestJSON: '',
   };
 
   private rules: any = {
     apiPath: [{ required: true, message: '请输入请求地址', trigger: 'blur', }],
-    rspJson: [{ required: true, message: '请输入响应JSON', trigger: 'blur', }],
+    responseJSON: [{ required: true, message: '请输入Response JSON', trigger: 'blur', }],
+  };
+
+  private formOut = {
+    name: '',
+    decApiCode: '',
+    exDefApiCode: `
+export const distribute = (params: any): D.IDistributeRsp =>
+  http.post('xsea/scene/distribute', params) as any;
+`,
+    exUseApiCode: '',
+    exImportCode: '',
   };
 
   private handleGenerateClick() {
-    (this.$refs.formIn as any).validate((valid: any) => {
-      console.log(valid);
-      if (valid) {
-        this.formOut = Generate(this.formIn, this.config);
-        this.$notification.success({
-          message: 'API代码生成成功',
-          description: '可根据需要复制到项目中',
-        });
-      }
-      return !!valid;
-    });
+    // 生成名称
+    this.formOut.name = this.getLastNameByPath(this.formIn.apiPath);
+    // 生成Response声明代码
+    this.formOut.decApiCode = '';
+    const structRsp = this.shuji.Infer(
+      `${this.formOut.name}Rsp`,
+      JSON.parse(this.formIn.responseJSON),
+    );
+    this.formOut.decApiCode += structRsp.TsTestCode;
+    // 生成Request声明代码
+    if (this.formIn.requestJSON.trim()) {
+      const structReq = this.shuji.Infer(
+        `${this.formOut.name}Req`,
+        JSON.parse(this.formIn.requestJSON),
+      );
+      this.formOut.decApiCode += `\n\n${structReq.TsTestCode}`;
+    }
+  }
+
+  private getLastNameByPath(pathStr: string) {
+    const segs = pathStr.split(/[\\\/]+/);
+    return segs[segs.length - 1] || 'xxx';
+  }
+
+  private getDefApiCode(
+    apiPath: string,
+  ) {
+
+  }
+
+  private get autoApiName() {
+    const segs = this.formIn.apiPath.split(/[\\\/]+/);
+    return segs[segs.length - 1] || 'myApi';
   }
 
   public render(): VNode {
@@ -76,17 +88,21 @@ export default class XTsApiHelper extends Vue {
                 v-model={this.formIn.apiPath}
                 placeholder="请输入请求地址 如：xsea/scene/querySceneDetail">
                 <a-select
-                  v-model={this.formIn.apiMethod}
                   slot="addonBefore"
                   default-value="post"
                   style="width: 90px">
-                  <a-select-option value="post">POST</a-select-option>
-                  <a-select-option value="get">GET</a-select-option>
-                  <a-select-option value="put">PUT</a-select-option>
-                  <a-select-option value="delete">DELETE</a-select-option>
-                  <a-select-option value="patch">PATCH</a-select-option>
-                  <a-select-option value="head">HEAD</a-select-option>
-                  <a-select-option value="options">OPTIONS</a-select-option>
+                  <a-select-option value="post">
+                    POST
+                  </a-select-option>
+                  <a-select-option value="get">
+                    GET
+                  </a-select-option>
+                  <a-select-option value="put">
+                    PUT
+                  </a-select-option>
+                  <a-select-option value="delete">
+                    DELETE
+                  </a-select-option>
                 </a-select>
               </a-input>
             </a-form-model-item>
@@ -95,21 +111,21 @@ export default class XTsApiHelper extends Vue {
               prop="responseJSON">
               <XCodeEditor
                 lang="json"
-                v-model={this.formIn.rspJson}
+                v-model={this.formIn.responseJSON}
               />
             </a-form-model-item>
             <a-form-model-item
-              label="Request JSON（接口请求的JSON数据，如不需要建立入参模型可不填）"
+              label="Request JSON（接口请求的JSON数据，可不填）"
               prop="requestJSON">
               <XCodeEditor
                 lang="json"
-                v-model={this.formIn.reqJson}
+                v-model={this.formIn.requestJSON}
               />
             </a-form-model-item>
           </a-form-model>
         </div>
         <div class={style.middle}>
-          <a-affix offsetTop={223}>
+          <a-affix offsetTop={203}>
             <a-button
               type="primary"
               onClick={this.handleGenerateClick}>
@@ -126,7 +142,7 @@ export default class XTsApiHelper extends Vue {
               label="生成名称"
               prop="name">
               <a-input
-                v-model={this.formIn.customName}
+                v-model={this.formOut.name}
                 placeholder="请输入定制化名称"
               />
             </a-form-model-item>
@@ -134,28 +150,28 @@ export default class XTsApiHelper extends Vue {
               label="类型声明代码">
               <XCodeEditor
                 lang="typescript"
-                v-model={this.formOut.decCode}
+                v-model={this.formOut.decApiCode}
               />
             </a-form-model-item>
             <a-form-model-item
               label="代码样例（定义API）">
               <XCodeEditor
                 lang="typescript"
-                v-model={this.formOut.defCode}
+                v-model={this.formOut.exDefApiCode}
               />
             </a-form-model-item>
             <a-form-model-item
               label="代码样例（使用API）">
               <XCodeEditor
                 lang="typescript"
-                v-model={this.formOut.useCode}
+                v-model={this.formOut.exUseApiCode}
               />
             </a-form-model-item>
             <a-form-model-item
               label="代码样例（引入具体类型）">
               <XCodeEditor
                 lang="typescript"
-                v-model={this.formOut.importCode}
+                v-model={this.formOut.exImportCode}
               />
             </a-form-model-item>
           </a-form-model>
